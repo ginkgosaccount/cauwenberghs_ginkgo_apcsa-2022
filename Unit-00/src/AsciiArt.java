0e8eab55ea1123a42e46059048c086d2ba209fbc
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-20 10:00:03 -0800

    Update AsciiArt.java

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -10,8 +10,8 @@ public class AsciiArt
 {
 	public static void main ( String[] args )
 	{
-		System.out.println("Your Name \n\n" );
-		System.out.println("What type of ANIMAL YOU WILL DRAW" );
+		System.out.println("Ginkgo \n\n" );
+		System.out.println("Self Portrait" );
 		System.out.println("\n\n\n\n" );
 
 		System.out.println("                /\\                " );
@@ -20,9 +20,9 @@ public class AsciiArt
 		System.out.println("             [------]             " );
 		//add other output
 
-		System.out.println(" \n\n\n\nHelpFul Hints" );
+		/*System.out.println(" \n\n\n\nHelpFul Hints" );
 		System.out.println("\\\\ draws one backslash on the screen!\n" );
 		System.out.println("\\\" draws one double quote on the screen!\n" );
-		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+		System.out.println("\\\' draws one single quote on the screen!\n" ); */
 	}
 }
