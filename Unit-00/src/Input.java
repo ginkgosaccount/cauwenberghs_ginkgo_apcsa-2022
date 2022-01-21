cce9d913cdce77046849d97b5e2b79f404a24465
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-20 23:55:54 -0800

    Update Input.java

--- Unit-00/src/Input.java
+++ Unit-00/src/Input.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Ginkgo Cauwenberghs
+//Date - 1/18/22
+//Class - AP CSA
+//Lab  - Unit 00
 
 import static java.lang.System.*;
 import java.util.Scanner;
@@ -20,24 +20,37 @@ public class Input
 		float floatOne, floatTwo;
 		short shortOne, shortTwo;
 
-
 		System.out.print("Enter an integer :: ");
 		intOne = keyboard.nextInt();
-
-
 		System.out.print("Enter an integer :: ");
 		intTwo = keyboard.nextInt();
-
-
-		//add in input for all variables
-
-
+		System.out.println();
+		System.out.print("Enter a double :: ");
+		doubleOne = keyboard.nextDouble();
+		System.out.print("Enter a double :: ");
+		doubleTwo = keyboard.nextDouble();
+		System.out.println();
+		System.out.print("Enter a float :: ");
+		floatOne = keyboard.nextFloat();
+		System.out.print("Enter a float :: ");
+		floatTwo = keyboard.nextFloat();
+		System.out.println();
+		System.out.print("Enter a short :: ");
+		shortOne = keyboard.nextShort();
+		System.out.print("Enter a short :: ");
+		shortTwo = keyboard.nextShort();
+		
 		System.out.println();
 		System.out.println("integer one = " + intOne );
 		System.out.println("integer two = " + intTwo );
-
-		//add in output for all variables
-
-
+		System.out.println();
+		System.out.println("double one = " + doubleOne );
+		System.out.println("double two = " + doubleTwo );
+		System.out.println();
+		System.out.println("float one = " + floatOne );
+		System.out.println("float two = " + floatTwo );
+		System.out.println();
+		System.out.println("short one = " + shortOne );
+		System.out.println("short two = " + shortTwo );
 	}
 }
