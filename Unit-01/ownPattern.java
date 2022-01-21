c96343d518ccc09f7cc2bc96f6d5b34c9453e8df
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-20 23:14:31 -0800

    Create ownPattern.java

---
+++ Unit-01/ownPattern.java
@@ -0,0 +1,24 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Ginkgo Cauwenberghs
+//Date - 1/18/22
+//Class - AP CSA
+//Lab  - Unit 01
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      StarsAndStripes thing = new StarsAndStripes();
+      
+      //my pattern is a minecra ft pixaxe
+      thing.printASmallBox();
+      thing.printTwentyDashes();
+      thing.printTwentyDashes();
+      thing.printASmallBox();
+      
+   }
+}
