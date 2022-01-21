dac2b987d04b9791b06372f298649a0ad91b3b84
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-20 23:23:16 -0800

    Update AsciiArt.java

--- Unit-00/src/AsciiArt.java
+++ Unit-00/src/AsciiArt.java
@@ -14,27 +14,22 @@ public class AsciiArt
 		System.out.println("Self Portrait" );
 		System.out.println("\n\n\n\n" );
 
-		
 		System.out.println("              _____________                " );
 		System.out.println("             /             \\              " );
-		System.out.println("            /   #      #    \\              " );
-		System.out.println("            |               |               " );
-		System.out.println("            |  \\________/   |               " );
-		System.out.println("            |_______________|                " );
-		System.out.println("            _____|      |_____                     " );
-		System.out.println("           |                  |          " );
+		System.out.println("            /   #      #    \\             " );
+		System.out.println("            |               |              " );
+		System.out.println("            |  \\________/   |             " );
+		System.out.println("            |_______________|              " );
+		System.out.println("            _____|      |_____             " );
+		System.out.println("           |                  |            " );
 		System.out.println("           |   |          |   |            " );
-		System.out.println("           |   |          |   |                 " );
-		System.out.println("           |   |__________|   |               " );
-		System.out.println("           |||||          |||||                       " );
-		System.out.println("               |    |     |                   " );
-		System.out.println("               |    |     |                   " );
-		System.out.println("            ___|____|_____|___                " );
-		System.out.println("            |_______|________|                  " );
-		System.out.println("                                            " );
-		System.out.println("                                            " );
-		
-		
-		
+		System.out.println("           |   |          |   |            " );
+		System.out.println("           |   |__________|   |            " );
+		System.out.println("           |||||          |||||            " );
+		System.out.println("               |    |     |                " );
+		System.out.println("               |    |     |                " );
+		System.out.println("            ___|____|_____|___             " );
+		System.out.println("            |_______|________|             " );
+		System.out.println("                                           " );	
 	}
 }
