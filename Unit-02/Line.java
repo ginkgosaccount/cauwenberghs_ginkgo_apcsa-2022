cdf61f419862306dbc6abb4cd5459580a989bbdb
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-21 22:19:57 -0800

    Update Line.java

--- Unit-02/Line.java
+++ Unit-02/Line.java
@@ -1,13 +1,14 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Ginkgo Cauwenberghs
+//Date - 1/21/22
 
 public class Line
 {
 	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		return 0;
+		double diffY = y2 - y1;
+		double diffX = x2 - x1;
+		return diffY/diffX;
 	}
-
 }
