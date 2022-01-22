7258ef27e9c45beb265a1b40657ccdf65a5efcf4
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-21 22:19:26 -0800

    Update LineRunner.java

--- Unit-02/LineRunner.java
+++ Unit-02/LineRunner.java
@@ -1,19 +1,19 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
+//Name - Ginkgo Cauwenberghs
+//Date - 1/21/22
+//Class - AP CSA
+//Lab  - Unit 02
 
 public class LineRunner
 {
-   public static void main( String[] args )
-   {
+   	public static void main( String[] args )
+   	{
+	    	Line Line = new Line();
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,9,14,2 ) );
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,18,3 ) );
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 6,4,2,2 ) );
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 4,4,5,3 ) );
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,1,2,9 ) );
 		System.out.printf( "Slope is :: %.2f\n" , Line.getSlope( 1,7,2,9 ) );
-		
 	}
 }
