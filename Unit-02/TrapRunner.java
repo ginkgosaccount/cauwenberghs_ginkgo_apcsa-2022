62f8f5b4c1f12dd71608223e929896b2e4984d03
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-21 22:06:31 -0800

    Update TrapRunner.java

--- Unit-02/TrapRunner.java
+++ Unit-02/TrapRunner.java
@@ -1,13 +1,15 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
+//Name - Ginkgo Cauwenberghs
+//Date - 1/21/22
+//Class - AP CSA
+//Lab  - Unit 02
 
 public class TrapRunner
 {
 	public static void main( String[] args )
 	{
+		Trap Trap = new Trap();
+
 		System.out.println( Trap.area( 3, 3, 3) );
 		System.out.println( Trap.area( 5, 6, 7) );
 		System.out.println( Trap.area( 7, 10, 6) );
@@ -16,16 +18,3 @@ public class TrapRunner
 		System.out.println( Trap.area( 11, 8, 5) );
 	}
 }
-
-
-/*
-EXPECTED OUTPUT
-
-9.0
-38.5
-51.0
-33.0
-34.0
-47.5
-
-*/
