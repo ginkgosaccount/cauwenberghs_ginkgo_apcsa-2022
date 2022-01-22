b79c374806b877d2661498df0ecec6b6cfd08daf
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-21 22:05:58 -0800

    Update Trap.java

--- Unit-02/Trap.java
+++ Unit-02/Trap.java
@@ -1,13 +1,16 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  - 
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
+//Name - Ginkgo Cauwenberghs
+//Date - 1/21/22
+//Class - AP CSA
+//Lab  - Unit 02
 
 public class Trap
 {
 	public static double area( int base1, int base2, int height )
 	{
-		return 0;
+		double newBase1 = base1;
+		double newBase2 = base2;
+		double newHeight = height;
+		return (newBase1 + newBase2)/2 * newHeight;
 	}
 }
