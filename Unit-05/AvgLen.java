9f599663e5530146f0cba4342640747955227580
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-27 13:20:20 -0800

    Create AvgLen.java

---
+++ Unit-05/AvgLen.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Ginkgo Cauwenberghs
+//Date - 1/27/22
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return 0;
+	}
+}
