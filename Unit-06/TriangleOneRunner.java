8aaf63ce2babda6b0d68560f177fdc917bb9a22f
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-31 13:06:54 -0800

    Update TriangleOneRunner.java

--- Unit-06/TriangleOneRunner.java
+++ Unit-06/TriangleOneRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Ginkgo Cauwenberghs
 
 import static java.lang.System.*;
 
@@ -8,6 +8,21 @@ public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		TriangleOne triangle = new TriangleOne();
+		
+		triangle.setWord("hippo");
+		triangle.print();
+		
+		triangle.setWord("abcd");
+		triangle.print();
+		
+		triangle.setWord("it");
+		triangle.print();
+		
+		triangle.setWord("a");
+		triangle.print();
+		
+		triangle.setWord("chicken");
+		triangle.print();
 	}
 }
