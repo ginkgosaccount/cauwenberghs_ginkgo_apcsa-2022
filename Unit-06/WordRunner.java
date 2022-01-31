55ee14529027425d821b2ab4a8f8bf31ac67d168
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-01-31 13:15:47 -0800

    Update WordRunner.java

--- Unit-06/WordRunner.java
+++ Unit-06/WordRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Ginkgo Cauwenberghs
 
 import static java.lang.System.*;
 
@@ -8,6 +8,21 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Word word = new Word();
+		
+		word.setString("Hello");
+		word.print();
+		
+		word.setString("World");
+		word.print();
+		
+		word.setString("JukeBox");
+		word.print();
+		
+		word.setString("TCEA");
+		word.print();
+		
+		word.setString("UIL");
+		word.print();
 	}
 }
