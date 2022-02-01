f53399454e1f017af084d3867617b8dd4ac44510
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-01 11:04:42 -0800

    Update LetterRemoverRunner.java

--- Unit-06/LetterRemoverRunner.java
+++ Unit-06/LetterRemoverRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ginkgo Cauwenberghs
 
 import static java.lang.System.*;
 
@@ -8,7 +8,21 @@ public class LetterRemoverRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases		
-											
+		LetterRemover thing = new LetterRemover();
+		
+		thing.setRemover("I am Sam I am",'a');
+		thing.print();
+		
+		thing.setRemover("ssssssssxssssesssssesss",'s');
+		thing.print();
+		
+		thing.setRemover("qwertyqwertyqwerty",'a');
+		thing.print();
+		
+		thing.setRemover("abababababa",'b');
+		thing.print();
+		
+		thing.setRemover("abaababababa",'x');
+		thing.print();									
 	}
 }
