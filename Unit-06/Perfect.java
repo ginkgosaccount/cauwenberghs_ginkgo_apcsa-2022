65fee3f334897ae0805867ad4d06f5cf501c4c84
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-01 11:11:30 -0800

    Update Perfect.java

--- Unit-06/Perfect.java
+++ Unit-06/Perfect.java
@@ -6,15 +6,32 @@ import static java.lang.System.*;
 
 public class Perfect
 {
-   private int number;
+   	private int number;
 
-	//add constructors
+	public Perfect()
+	{
+		number = 0;
+	}
+	
+	public Perfect(int num)
+	{
+		number = num;
+	}
 
-	//add a set method
+	public void setNumber(int num)
+	{
+		number = num;
+	}
 
 	public boolean isPerfect()
 	{
-		return false;
+		int
+		int
+		for (int i=1;i<number;i++)
+		{
+			if (number%i == 0)
+				sum += i;
+		}
 	}
 
 	//add a toString	
