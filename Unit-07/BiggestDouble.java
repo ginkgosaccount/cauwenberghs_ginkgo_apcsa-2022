6c953ddd0961a1885efeee43348120c7589a4d75
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-03 10:42:15 -0800

    Update BiggestDouble.java

--- Unit-07/BiggestDouble.java
+++ Unit-07/BiggestDouble.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 // www.apluscompsci.com
-//Name -  
+//Name -  Ginkgo Cauwenberghs
 
 import static java.lang.System.*;
 
@@ -10,24 +10,38 @@ public class BiggestDouble
 
 	public BiggestDouble()
 	{
-		this(0,0,0,0);
+		setDoubles(0,0,0,0);
 	}
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double biggest = one;
+		
+		if (two > biggest)
+			biggest = two;
+		if (three > biggest)
+			biggest = three;
+		if (four > biggest)
+			biggest = four;
+		
+		return biggest;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return "" + one + two + three + four;
 	}
 }
