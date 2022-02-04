b30da0717dab202eb18e16233f8a278e6a2d8b35
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-04 13:48:54 -0800

    Update TriangleFive.java

--- Unit-07/TriangleFive.java
+++ Unit-07/TriangleFive.java
@@ -6,8 +6,8 @@ import static java.lang.System.*;
 
 public class TriangleFive
 {
-   private char letter;
-   private int amount;
+   	private char letter;
+   	private int amount;
 
 	public TriangleFive()
 	{
