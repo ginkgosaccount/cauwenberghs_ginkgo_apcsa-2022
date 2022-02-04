7888d6672dfcc2c58c420a0607c378778badef08
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-04 13:47:39 -0800

    Update TriangleFiveRunner.java

--- Unit-07/TriangleFiveRunner.java
+++ Unit-07/TriangleFiveRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Ginkgo Cauwenberghs
 
 import static java.lang.System.*;
 
@@ -8,5 +8,21 @@ public class TriangleFiveRunner
 {
    public static void main(String args[])
    {
-	}
+	   TriangleFive triange = new TriangleFive();
+	   
+	   triangle.setLetter('C'); triange.setAmount(4);
+	   triangle.print();
+	   
+	   triangle.setLetter('A'); triange.setAmount(5);
+	   triangle.print();
+	   
+	   triangle.setLetter('B'); triange.setAmount(6);
+	   triangle.print();
+	   
+	   triangle.setLetter('X'); triange.setAmount(7);
+	   triangle.print();
+	   
+	   triangle.setLetter('Z'); triange.setAmount(8);
+	   triangle.print();
+   }
 }
