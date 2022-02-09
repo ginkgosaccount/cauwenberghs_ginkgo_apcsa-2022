a6684c8d887a6bbc16cf150dfc03ac76eaf7cd9f
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-02-08 23:47:58 -0800

    Update RockPaperScissors.java

--- Unit-07/RockPaperScissors.java
+++ Unit-07/RockPaperScissors.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 // www.apluscompsci.com
-//Name -  
+//Name -  Ginkgo Cauwenberghs
 
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -12,25 +12,57 @@ public class RockPaperScissors
 
 	public RockPaperScissors()
 	{
+		setPlayers(' ');
 	}
 
 	public RockPaperScissors(String player)
 	{
+		setPlayers(player);
 	}
 
 	public void setPlayers(String player)
 	{
+		if (player == 'R')
+			playChoice = "rock";
+		if (player == 'P')
+			playChoice = "paper";
+		if (player == 'S')
+			playChoice = "scissors";
+		
+		int random = (int) (math.random()*3);
+		if (random == 0)
+			compChoice = "rock";
+		if (random == 1)
+			compChoice = "paper";
+		if (random == 2)
+			compChoice = "scissors";
+		
 	}
 
 	public String determineWinner()
 	{
-		String winner="";
-		return winner;
+		if (playChoice == compChoice)
+			return "!Draw Game!";
+
+		if (playChoice.equals("rock") && compChoice.equals("paper")
+		    	return "!Computer wins - paper covers rock!\n";
+		if (playChoice.equals("rock") && compChoice.equals("scissors")
+		    	return "!Player wins - rock breaks scissors!\n";
+		if (playChoice.equals("paper") && compChoice.equals("rock")
+		    	return "!Player wins - paper covers rock!\n";
+		if (playChoice.equals("paper") && compChoice.equals("scissors")
+		    	return "!Computer wins - scissors cuts paper!\n";
+		if (playChoice.equals("scissors") && compChoice.equals("rock")
+		    	return "!Copmuter wins - rock breaks scissors!\n";
+		if (playChoice.equals("scissors") && compChoice.equals("paper")
+		    	return "!Player wins - scissors cuts paper!\n";
+
 	}
 
-	public String toString()
+	public void print()
 	{
-		String output="";
-		return output;
+		out.printf("Player had %s\n", playChoice);
+		out.printf("Computer had %s\n", compChoice):
+		out.print(determineWinner());
 	}
 }
