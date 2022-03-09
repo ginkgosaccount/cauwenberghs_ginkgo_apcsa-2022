07c3b461acc6a8d024da7a69de9f75bb352c0176
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-03-09 11:41:11 -0800

    Update Skeleton.java

--- Unit-12/Skeleton.java
+++ Unit-12/Skeleton.java
@@ -6,11 +6,52 @@ import static java.lang.System.*;
 
 public class Skeleton implements Monster
 {
-	//add instance variables	
+	//add instance variables
+	private int size;
+	private String name;
 
 	//add a constructor
+	public Skeleton(String n, int s)
+	{
+		size = s;
+		name = n;
+	}
 
 	//add code to implement the Monster interface
+	public int getHowBig()
+	{
+		return size;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean isBigger(Monster other)
+	{
+		if (size > other.getHowBig())
+			return true;
+		return false;
+	}
+	
+	public boolean isSmaller(Monster other)
+	{
+		if (size < other.getHowBig())
+			return true;
+		return false;
+	}
+	
+	public boolean namesTheSame(Monster other);
+	{
+		if (name.equals(other.getName()))
+			return true;
+		return false;
+	}
 
 	//add a toString
+	public String toString()
+	{
+		return "" + name + " " + size;
+	}
 }
