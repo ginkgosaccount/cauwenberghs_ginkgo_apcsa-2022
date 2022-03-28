c99a2a3b226fa6435ddba29d7ea5cc78078c2fc9
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-03-28 10:13:30 -0700

    Update Recursion234Runner.java

--- Unit-14/Recursion234Runner.java
+++ Unit-14/Recursion234Runner.java
@@ -8,7 +8,22 @@ public class Recursion234Runner
 {
 	public static void main(String args[])
 	{
+		RecursionFunOne RecursionFunOne = new RecursionFunOne();
+		
 		System.out.println(RecursionFunOne.countOddDigits(4532));
+		System.out.println(RecursionFunOne.countOddDigits(11145322));
+		System.out.println(RecursionFunOne.countOddDigits(224532714));
+		System.out.println(RecursionFunOne.countOddDigits(2468));
+		System.out.println(RecursionFunOne.countOddDigits(13579));
+
+		RecursionFunThree RecursionFunThree = new RecursionFunThree();
+		
+		System.out.println(RecursionFunThree.luckyThrees(3));
+		System.out.println(RecursionFunThree.luckyThrees(31332));
+		System.out.println(RecursionFunThree.luckyThrees(134523));
+		System.out.println(RecursionFunThree.luckyThrees(3113));
+		System.out.println(RecursionFunThree.luckyThrees(13331));
+		System.out.println(RecursionFunThree.luckyThrees(777337777));
 
 		//add more test cases
 	}
