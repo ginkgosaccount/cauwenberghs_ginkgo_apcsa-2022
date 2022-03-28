41ed2bc1cf30ca34f23d5815b74fb7f5576b2e09
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-03-28 10:19:57 -0700

    Update TotalRow.java

--- Unit-14/TotalRow.java
+++ Unit-14/TotalRow.java
@@ -8,7 +8,18 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
-    	//add code here
-		return null;
+    	List<Integer> thing = new List<Integer>;
+	
+	for (int i=0;i<m.length();i++)
+	{
+		int x=0;
+		for (int y=0;y<m[i].length();y++)
+		{
+			x+= m[i][y];
+		}
+		thing.add(x);
+	}
+	
+	return thing;
     }
 }
