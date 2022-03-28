0d8604b8d1ba7044726524e20c35b47a8be94bea
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-03-28 11:06:11 -0700

    Update GridRunner.java

--- Unit-14/GridRunner.java
+++ Unit-14/GridRunner.java
@@ -11,5 +11,12 @@ public class GridRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
+		String[] thing = {"a","b","c","7","9","x","2"};
+		
+		Grid x = new Grid(20,20,thing);
+		
+		System.out.print(x);
+		System.out.println("");
+		System.out.println(x.findMax + "occurs the most.")
 	}
 }
