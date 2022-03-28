3c6fb4d93b06eb4a0646ec5882cdfe5c23e60399
ginkgosaccount <97690306+ginkgosaccount@users.noreply.github.com>
2022-03-28 10:28:02 -0700

    Update TotalRowRunner.java

--- Unit-14/TotalRowRunner.java
+++ Unit-14/TotalRowRunner.java
@@ -6,7 +6,15 @@ public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code here			
+		TotalRow x = new TotalRow();
+		
+		int[][] one = { {1,2,3},{5,5,5,5} };
+		int[][] two = { {1,2,3},{5},{1},{2,2} };
+		int[][] three = { {1,2},{5,5},{5,5},{4,5,6,7},{123124,12312} };
+		
+		System.out.println("Row totals are :: " + x.getRowTotals(one));
+		System.out.println("Row totals are :: " + x.getRowTotals(two));
+		System.out.println("Row totals are :: " + x.getRowTotals(three));
 	}
 }
 
